8afe4df7e5e550fdd10826af1cdad55ff51093f1
Sergey-Markov <1nativefl@gmail.com>
2022-01-12 22:49:34 +0200

    add Container to App.js

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,5 +1,5 @@
 import s from "./Container.module.css";
 
-export default function Container() {
-  return <section className={s.notes_table}></section>;
+export default function Container({ children }) {
+  return <section className={s.notes_table}>{children}</section>;
 }
