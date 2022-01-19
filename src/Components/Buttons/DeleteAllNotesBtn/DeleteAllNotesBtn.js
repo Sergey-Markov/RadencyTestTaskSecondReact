056a4b82375b0e08c7e06a50ddf58a2d96de76f3
Sergey-Markov <1nativefl@gmail.com>
2022-01-19 12:25:43 +0200

    create logic on DeleteAllNotesBtn()

--- src/Components/Buttons/DeleteAllNotesBtn/DeleteAllNotesBtn.js
+++ src/Components/Buttons/DeleteAllNotesBtn/DeleteAllNotesBtn.js
@@ -1,12 +1,20 @@
 import CommunButton from "../CommunButton.js";
 import { BsFillTrashFill } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { deleteAllNotes } from "../../../Redux/actions.js";
 
 export default function DeleteAllNotesBtn() {
+  const dispatch = useDispatch();
+  function deleteAll() {
+    console.log("click");
+    dispatch(deleteAllNotes());
+  }
   return (
     <CommunButton
       type="button"
       className="notes_table-head--btn"
-      id="deleteAllNotes"
+      id="deleteAll"
+      onClick={deleteAll}
     >
       <BsFillTrashFill />
     </CommunButton>
