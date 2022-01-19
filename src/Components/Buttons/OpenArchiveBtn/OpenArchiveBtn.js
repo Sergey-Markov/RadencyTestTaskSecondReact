d812aa348bc17b4053228bbb70d1b184c86321f1
Sergey-Markov <1nativefl@gmail.com>
2022-01-19 12:26:22 +0200

    create logic on OpenArchiveBtn

--- src/Components/Buttons/OpenArchiveBtn/OpenArchiveBtn.js
+++ src/Components/Buttons/OpenArchiveBtn/OpenArchiveBtn.js
@@ -1,13 +1,13 @@
 import { BsFillArchiveFill } from "react-icons/bs";
 import CommunButton from "../CommunButton.js";
 
-export default function OpenArchiveBtn({ setStatus }) {
+export default function OpenArchiveBtn({ onClick }) {
   return (
     <CommunButton
       type="button"
       className="notes_table-head--btn"
       id="showArchTable"
-      onClick={() => setStatus()}
+      onClick={onClick}
     >
       <BsFillArchiveFill />
     </CommunButton>
